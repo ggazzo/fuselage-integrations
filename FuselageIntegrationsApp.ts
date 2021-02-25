a2a1815d604a74b3b3b814e0224c35d1ad1c7abb
Diego Sampaio <chinello@gmail.com>
2021-02-25 17:20:12 -0300

    Use configured rooms for teams

--- FuselageIntegrationsApp.ts
+++ FuselageIntegrationsApp.ts
@@ -6,12 +6,33 @@ import {
 import { ApiSecurity, ApiVisibility } from '@rocket.chat/apps-engine/definition/api';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { SettingType } from '@rocket.chat/apps-engine/definition/settings';
 import { WebHook } from './endpoints/webhook';
 
 export class FuselageIntegrationsApp extends App {
+    private TEAM_ROOMS: Record<string, string> = {};
+
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
+
+    public async onEnable(environmentRead) {
+        const teamRooms = await environmentRead.getSettings().getValueById('TEAM_ROOM');
+
+        // TODO update TEAM_ROOMS every time the setting is saved
+        teamRooms.split(',').forEach((teamRoom) => {
+            const [team, room] = teamRoom.split(':');
+            this.TEAM_ROOMS[team] = room;
+        });
+
+        return true;
+    }
+
+    public getRoomForTeam(team: string): string {
+        // console.log('this.TEAM_ROOMS ->', team, this.TEAM_ROOMS);
+        return this.TEAM_ROOMS[team];
+    }
+
     protected async extendConfiguration(configuration: IConfigurationExtend): Promise<void> {
         await configuration.api.provideApi({
             visibility: ApiVisibility.PUBLIC,
@@ -19,6 +40,15 @@ export class FuselageIntegrationsApp extends App {
             endpoints: [
                 new WebHook(this),
             ],
+        });
+
+        configuration.settings.provideSetting({
+            id: 'TEAM_ROOM',
+            type: SettingType.STRING,
+            packageValue: '',
+            required: true,
+            public: false,
+            i18nLabel: 'TEAM_ROOM',
         });
     }
 }
--- endpoints/webhook.ts
+++ endpoints/webhook.ts
@@ -4,7 +4,7 @@ import { IMessage } from '@rocket.chat/apps-engine/definition/messages';
 import { RocketChatAssociationModel, RocketChatAssociationRecord } from '@rocket.chat/apps-engine/definition/metadata';
 import { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
 import { BlockElementType, IImageElement, TextObjectType } from '@rocket.chat/apps-engine/definition/uikit';
-
+import type { FuselageIntegrationsApp } from '../FuselageIntegrationsApp';
 
 enum EVENT_TYPES {
     PULL_REQUEST = 'pull_request',
@@ -14,7 +14,7 @@ enum EVENT_TYPES {
 type User = {
     login: string;
     avatar_url: string;
-}
+};
 
 type PullRequest =  {
     id: number;
@@ -24,24 +24,23 @@ type PullRequest =  {
     body: string;
     user: User;
     number: number;
-    requested_teams: {
+    requested_teams: Array<{
         name: string;
         slug: string;
-    } []
-}
-
+    }>;
+};
 
 type Reviews = {
     id: string;
     state: 'APPROVED' | 'CHANGES_REQUESTED';
     user: User;
-}
+};
 
 type GithubActionPullRequest = {
     action: 'opened' | 'edited' | 'closed' | 'assigned' | 'unassigned' | 'review_requested' | 'review_request_removed' | 'ready_for_review' | 'labeled' | 'unlabeled' | 'synchronize' | 'locked' | 'unlocked';
     number: number;
     pull_request: PullRequest;
-}
+};
 
 type GithubActionPullRequestReview = {
     action: 'submitted' | 'dismissed';
@@ -50,152 +49,185 @@ type GithubActionPullRequestReview = {
         id: 'string';
         user: User
         state: 'approved';
-    }
-}
+    };
+};
 
 type PullRequestRelation = {
     mid: string;
-}
-
-
+    rid: string;
+};
 
 export class WebHook extends ApiEndpoint {
     public path: string = 'webhook';
+
+    public app: FuselageIntegrationsApp;
+
     public get(request: IApiRequest, endpoint: IApiEndpointInfo, read: IRead, modify: IModify, http: IHttp, persis: IPersistence) {
         this.app.getLogger().log(request);
         return this.success({ ok: true });
     }
-    public async post(request: IApiRequest, endpoint: IApiEndpointInfo, read: IRead, modify: IModify, http: IHttp, persis: IPersistence): Promise<IApiResponse> {
 
-        const room = await read.getRoomReader().getById('PdudbceedgKRq6qGX');
-
-        if(!room) {
-            return this.success({ ok: true });
-        }
+    public async post(request: IApiRequest, endpoint: IApiEndpointInfo, read: IRead,
+                      modify: IModify, http: IHttp, persis: IPersistence): Promise<IApiResponse> {
 
         const userApp = await read.getUserReader().getAppUser(this.app.getID());
 
-        const makeBlocks = (reviews: Reviews[], pr: PullRequest) => {
+        const makeBlocks = (reviews: Array<Reviews>, pr: PullRequest) => {
             const blockBuilder = modify.getCreator().getBlockBuilder();
 
             blockBuilder.addSectionBlock({
                 text: {
                     type: TextObjectType.MARKDOWN,
-                    text: `*${ pr.title }* [#${pr.number}](${ pr.html_url })`
+                    text: `*${ pr.title }* [#${pr.number}](${ pr.html_url })`,
                 },
                 accessory: {
                     type: BlockElementType.IMAGE,
                     imageUrl: pr.user.avatar_url,
                     altText: pr.user.login,
-                }
+                },
             });
 
-            if(reviews.length) {
+            if (reviews.length) {
 
-                const appproved: IImageElement[] = reviews.filter(review => review.state ==='APPROVED' ).map(review => ({
-                    type: BlockElementType.IMAGE,
-                    imageUrl: review.user.avatar_url,
-                    altText: review.user.login,
-                }))
-
-                appproved.length && blockBuilder.addContextBlock({ elements: [
-                    {
-                        type: TextObjectType.MARKDOWN,
-                        text: "*Approved*"
-                    },
-                    ...appproved,
-                ] })
-
-                const changesRequested: IImageElement[] = reviews.filter(review => review.state === 'CHANGES_REQUESTED' ).map(review => ({
+                const appproved: Array<IImageElement> = reviews.filter((review) => review.state === 'APPROVED' ).map((review) => ({
                     type: BlockElementType.IMAGE,
                     imageUrl: review.user.avatar_url,
                     altText: review.user.login,
                 }));
 
-                changesRequested.length && blockBuilder.addContextBlock({ elements: [
-                    {
-                        type: TextObjectType.MARKDOWN,
-                        text: "*Changes Requested*"
-                    },
-                    ...changesRequested,
-                ] })
+                if (appproved.length) {
+                    blockBuilder.addContextBlock({ elements: [
+                        {
+                            type: TextObjectType.MARKDOWN,
+                            text: '*Approved*',
+                        },
+                        ...appproved,
+                    ] });
+                }
+
+                const changesRequested: Array<IImageElement> = reviews.filter((review) => review.state === 'CHANGES_REQUESTED' ).map((review) => ({
+                    type: BlockElementType.IMAGE,
+                    imageUrl: review.user.avatar_url,
+                    altText: review.user.login,
+                }));
+
+                if (changesRequested.length) {
+                    blockBuilder.addContextBlock({ elements: [
+                        {
+                            type: TextObjectType.MARKDOWN,
+                            text: '*Changes Requested*',
+                        },
+                        ...changesRequested,
+                    ] });
+                }
             }
 
             return blockBuilder;
-        }
+        };
 
+        const getRooms = (requestedTeams) => requestedTeams.map(({ name }) => this.app.getRoomForTeam(name)).filter(Boolean);
 
-
-        const fetchAndPersist = async (pull_request: PullRequest) => {
-
+        const fetchAndPersist = async (pull_request: PullRequest): Promise<void> => {
             try {
-
-                if(!userApp) {
+                if (!userApp) {
                     return;
                 }
-
 
                 const association = new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, pull_request.id.toString());
-                const [pr] = await read.getPersistenceReader().readByAssociation(association) as Array<PullRequestRelation>;
-                if(!pr && !pull_request.requested_teams.find(({ name }) => name === 'Frontend')) {
+                const [assocs] = await read.getPersistenceReader().readByAssociation(association) as Array<Array<PullRequestRelation>>;
+
+                const rooms: Array<{
+                    mid?: string;
+                    rid: string;
+                }> = [];
+                if (assocs) {
+                    assocs.forEach((room) => rooms.push(room));
+                }
+
+                getRooms(pull_request.requested_teams).forEach((room) => {
+                    if (rooms.find(({ rid }) => rid === room)) {
+                        return;
+                    }
+                    rooms.push({ rid: room });
+                });
+
+                if (rooms.length === 0) {
                     return;
                 }
-                const message = pr?.mid ? await (await modify.getUpdater().message(pr.mid, userApp)).setEditor(userApp) : await modify.getCreator().startMessage().setUsernameAlias('Fudelage')
-                .setEmojiAvatar(':illuminati:').setRoom(room);
-                const finisher = pr?.mid ? modify.getUpdater() : modify.getCreator();
 
-                const request = await (await http.get(`${pull_request.url}/reviews`, {
-                    headers: {
-                        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'
+                for (const assoc of rooms) {
+                    const { mid, rid } = assoc;
+
+                    const room = await read.getRoomReader().getById(rid);
+                    if (!room) {
+                        return;
                     }
-                }));
 
+                    const message = mid
+                        ? await (await modify.getUpdater().message(mid, userApp)).setEditor(userApp)
+                        : await modify.getCreator().startMessage()
+                            .setEmojiAvatar(':mage:').setRoom(room);
 
-                const reviews: Reviews[] = request.data;
+                    const finisher = mid ? modify.getUpdater() : modify.getCreator();
 
-                const blockBuilder = makeBlocks(reviews, pull_request);
+                    const response = await (await http.get(`${pull_request.url}/reviews`, {
+                        headers: {
+                            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
+                        },
+                    }));
 
-                message.setBlocks(blockBuilder);
+                    const reviews: Array<Reviews> = response.data;
 
-                const mid = await finisher.finish(message);
+                    const blockBuilder = makeBlocks(reviews, pull_request);
 
-                if(!pr) {
-                    await persis.createWithAssociation({ mid }, association);
+                    message.setBlocks(blockBuilder);
+
+                    const newId = await finisher.finish(message);
+                    if (newId) {
+                        assoc.mid = newId;
+                    }
                 }
+
+                if (!assocs) {
+                    await persis.createWithAssociation(rooms, association);
+                } else {
+                    await persis.updateByAssociation(association, rooms);
+                }
+
+                return;
             } catch (error) {
                 this.app.getLogger().error(error);
             }
-        }
+        };
 
         const handlePullRequest = async (): Promise<IApiResponse> => {
             const { action , pull_request }: GithubActionPullRequest = JSON.parse(request.content.payload);
 
-            switch(action) {
+            switch (action) {
                 case 'review_requested':
                 case 'opened':
                 case 'edited':
-                    this.app.getLogger().log('here');
+                    // this.app.getLogger().log('here');
                     await fetchAndPersist(pull_request);
-                break;
+                    break;
             }
             return this.success({ ok: true });
-        }
+        };
 
         const handlePullRequestReview = async (): Promise<IApiResponse> => {
             const { action, review , pull_request }: GithubActionPullRequestReview = JSON.parse(request.content.payload);
 
-
-            switch(action) {
+            switch (action) {
                 case 'dismissed':
                 case 'submitted':
 
                     await fetchAndPersist(pull_request);
-                break;
+                    break;
             }
             return this.success({ ok: true });
-        }
+        };
 
-        this.app.getLogger().log(request);
+        // this.app.getLogger().log(request);
         if (request.headers['x-github-event'] === EVENT_TYPES.PULL_REQUEST) {
             return handlePullRequest();
         }
